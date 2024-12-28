8781b722943b77af42d441d8a6128a1e5d75b78f
Rodrigo Pinto <103071413+rodrigoamadeu@users.noreply.github.com>
2024-12-28 14:07:46 +0000

    criando seção questions and answers

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -12,7 +13,18 @@ document.addEventListener('DOMContentLoaded', function() {
             
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+
 })
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -21,8 +33,6 @@ function removeBotaoAtivo() {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
-
-
 
 function escondeTodasAbas(){
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
